8f23cdac06a03f58cd4511c37be1889a170c8776
Z1M1NG <zimingx3@gmail.com>
2026-01-15 18:32:50 +0800

    improved scoring

--- src/scoring_engine.py
+++ src/scoring_engine.py
@@ -30,7 +30,8 @@ TRUSTED_PATHS = [
     r"c:\program files\windows defender",
     r"c:\program files (x86)\common files",
     r"c:\program files\tailscale",
-    r"c:\program files (x86)\Microsoft\Edge\Application"
+    r"c:\program files\teamviewer",
+    r"c:\program files\WindowsApps"
 ]
 
 # --- 3. HELPER FUNCTIONS ---
